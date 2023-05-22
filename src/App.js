32b66ea5c804360f1d314f6f3ac01fd831c91d69
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-05-21 23:10:17 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Header from "./Header";
+
+function App() {
+    // return "Hello! Social Platform!"
+    return <Header />;
+}
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+// npm install sematic-ui-react semantic-ui-css react-router-dom
+import { Menu } from "semantic-ui-react";
+
+function Header() {
+    return <Menu>
+        <Menu.Item></Menu.Item>
+    </Menu>
+}
+
+export default Header;
