d7e6326feb80f4baa300393c399a85bfa825a7b5
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-05-22 00:29:08 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./Header";
+
+import Signin from "./pages/Signin";
 
 function App() {
     // return "Hello! Social Platform!"
-    return <Header />;
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element="Homepage"></Route>
+                <Route path="/signin" element={<Signin />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,23 @@
 // npm install sematic-ui-react semantic-ui-css react-router-dom
-import { Menu } from "semantic-ui-react";
+import { Menu, Search } from "semantic-ui-react";
+import { Link } from "react-router-dom";  // need use (BrwoserRouter) before using Link 
 
 function Header() {
+
+    // navbar
+    // to="/" is homepage
+    // click Register/Login leads to /signin
+    // click Social Platform leads to homepage
     return <Menu>
-        <Menu.Item></Menu.Item>
+        <Menu.Item as={Link} to="/">  
+            Social Platform
+        </Menu.Item>
+        <Menu.Item>
+            <Search/>
+        </Menu.Item>
+        <Menu.Menu position="right">
+            <Menu.Item as={Link} to="/signin">Register/Login</Menu.Item>
+        </Menu.Menu>
     </Menu>
 }
 
