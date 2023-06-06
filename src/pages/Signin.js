fd5105f400683bdd6ad58de3267037bfb024a223
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-06-05 22:50:41 -0400

    Update Signin.js

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,9 +1,19 @@
 // https://www.youtube.com/watch?v=5jCTQx6TPTo&list=PLddLA9QpG2T2__tPfi6nwaL8Rf_wWQaz7&index=5
 
+import React from "react";
+
 // use Menu, Form, Container from semantic-ui to create signin page
 import {Menu, Form, Container} from "semantic-ui-react";
 
-import React from "react";
+// react-router-dom version 6 need to use useNavigate (not useHistory)
+import { useNavigate } from "react-router-dom";
+
+// import firebase
+import firebase from "../utils/firebase";
+
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
 
 function Signin() {
     // return "Hello, signing!";
@@ -12,8 +22,37 @@ function Signin() {
     const [activeItem, setActiveItem] = React.useState("register");
 
     // constance to set default state of email and password
-    const [email, setEmail] = React.useState("")
-    const [password, setPassword] = React.useState("")
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    // usehistory from useHistory
+    const navigate = useNavigate();
+
+    function onSubmit() {
+        if (activeItem === "register") {
+            // create an account
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(email, password)
+                .then(() => {
+                    // redirect to the homepage
+                    // history.push('/');
+                    // react-router-dom version 6 need to use navigate
+                    navigate("/");
+                });
+        } else if (activeItem === "signin") {
+            // sign in
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(email, password)
+                .then(() => {
+                    // redirect to the homepage
+                    // history.push('/');
+                    // react-router-dom version 6 need to use navigate
+                    navigate("/");
+                });
+        }
+    }
 
     return (
         <Container>
@@ -32,7 +71,7 @@ function Signin() {
                 </Menu.Item>
             </Menu>
 
-            <Form>
+            <Form onSubmit={onSubmit}>
                 <Form.Input 
                     label="Email" 
                     value={email} 
@@ -49,7 +88,7 @@ function Signin() {
                 <Form.Button>
                     {activeItem === "register" && "Register"}
                     {activeItem === "signin" && "Login"}
-                </Form.Button>
+                </Form.Button> 
             </Form>
         </Container>
     );
