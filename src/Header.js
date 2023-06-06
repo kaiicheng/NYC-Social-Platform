da12048b095aca2f9a2b51071536630818301f4a
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-06-06 12:02:18 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 // npm install sematic-ui-react semantic-ui-css react-router-dom
 import { Menu, Search } from "semantic-ui-react";
-import { Link } from "react-router-dom";  // need use (BrwoserRouter) before using Link 
+import { Link } from "react-router-dom";  // need to use (BrwoserRouter) before using Link 
+
+import React from "react";
+
+import firebase from "./utils/firebase";
 
 function Header() {
+
+    // monitoring the state of signed or not
+    // initializinig state, not sure signed or not => null
+    const [user, setUser] = React.useState(null);
+    // after signOut, the currentUser will become null
+    React.useEffect(() => {
+        firebase.auth().onAuthStateChanged((currentUser) => {
+            setUser(currentUser);
+        });
+    }, []);
 
     // navbar
     // to="/" is homepage
@@ -16,9 +30,27 @@ function Header() {
             <Search/>
         </Menu.Item>
         <Menu.Menu position="right">
-            <Menu.Item as={Link} to="/signin">Register/Login</Menu.Item>
-        </Menu.Menu>
-    </Menu>
+            {user ? (
+                <>
+                    <Menu.Item as={Link} to="/new-post">
+                        Post
+                    </Menu.Item>
+                    <Menu.Item as={Link} to="/my">
+                        Account
+                    </Menu.Item>
+                    {/* after signOut, the currentUser will become null */}
+                    <Menu.Item onClick={() => firebase.auth().signOut()}>
+                        Log out
+                    </Menu.Item>
+                    </>
+                ) : (
+                    <Menu.Item as={Link} to="/signin">
+                        Register/Login
+                    </Menu.Item>
+                )}
+            </Menu.Menu>
+        </Menu>
+    ;
 }
 
 export default Header;
