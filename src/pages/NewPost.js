0dd7b7cff6ee3c45ca3247d6ec363013e5984352
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-06-14 17:27:35 -0400

    Update NewPost.js

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,3 +1,6 @@
+// Solution to user does not have permission to access this object . Firebase storage
+// https://stackoverflow.com/questions/38671444/user-does-not-have-permission-to-access-this-object-firebase-storage-android
+
 import { Container, Header, Form, Image, Button } from "semantic-ui-react"; 
 
 import React from "react";
@@ -5,6 +8,9 @@ import React from "react";
 import "firebase/firestore";
 
 import firebase from "../utils/firebase";
+
+// need to add ../compat/..
+import "firebase/compat/storage";
 
 // react-router-dom version 6 need to use useNavigate (not useHistory)
 import { useNavigate } from "react-router-dom";
@@ -62,26 +68,38 @@ function NewPost() {
 
         // once submitted, user will create a posts object on firebase firestore
         const documentRef = firebase.firestore().collection("posts").doc()
-        documentRef.set({
-            title,
-            content,
-            topic: topicName,
-            createdAt: firebase.firestore.Timestamp.now(),
-            author: {  // map object on firebase firestore
-                // ||: or
-                displayName: firebase.auth().currentUser.displayName || "",
-                photoURL: firebase.auth().currentUser.photoURL || "",
-                uid: firebase.auth().currentUser.uid,
-                email: firebase.auth().currentUser.email,
-            },
-        }).then(() => {
-            // redirect to the homepage
-            // history.push('/');
-            // react-router-dom version 6 need to use navigate
-            navigate("/");
 
-            // loading time for submit
-            setIsLoading(false);
+        // create a ref for photo uploaded
+        const fileRef = firebase.storage().ref("post-images/" + documentRef.id)
+        // upload file of photo
+        const metadata = {
+            contentType: file.type
+        };
+        fileRef.put(file, metadata).then(() => {
+            fileRef.getDownloadURL().then((imageURL) => {
+                documentRef.set({
+                    title,
+                    content,
+                    topic: topicName,
+                    createdAt: firebase.firestore.Timestamp.now(),
+                    author: {  // map object on firebase firestore
+                        // ||: or
+                        displayName: firebase.auth().currentUser.displayName || "",
+                        photoURL: firebase.auth().currentUser.photoURL || "",
+                        uid: firebase.auth().currentUser.uid,
+                        email: firebase.auth().currentUser.email,
+                    },
+                    imageURL
+                }).then(() => {
+                    // redirect to the homepage
+                    // history.push('/');
+                    // react-router-dom version 6 need to use navigate
+                    navigate("/");
+        
+                    // loading time for submit
+                    setIsLoading(false);
+                });
+            })
         });
     }
 
